569278b5b6e03b9f77993d6d27b472f94cc08dbd
Philipp <barbey@iqo.uni-hannover.de>
2024-06-28 14:24:34 +0200

    set timer1 to external clock

--- src/hardware/setup.rs
+++ src/hardware/setup.rs
@@ -403,8 +403,7 @@ where
         // timer, but use maximum overflow period.
         let mut timer = timers::TimestampTimer::new(timer1);
 
-        // TODO: Check hardware synchronization of timestamping and the sampling timers
-        // for phase shift determinism.
+        timer.set_external_clock(timers::Prescaler::Div1);
 
         timer.set_period_ticks(u16::MAX);
 
